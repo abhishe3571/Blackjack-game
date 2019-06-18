4d5637b9a59f026b265b4e403700588653ac9e4a
Abhishek Singh <abhishek@gmail.com>
2019-06-18 16:09:36 +0530

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-let deck = [
-        "Ace of Spades",
-        "Two of Spades",
-        "Three of Spades"
-];
+let score = 1000;
 
-let playerCards = [ deck[0], deck[2] ];
-console.log("Welcome to Blackjack!");
+if (score >= 1000) {
+    score = score + 100;
+}
 
-
-console.log("You are dealt: ");
-console.log( " " + playerCards[0]);
-console.log( " " + playerCards[1]);
+console.log("Score is: ", score);
 
