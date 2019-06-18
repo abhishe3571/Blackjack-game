3c089548c236d6d4fa679a991e5ba3549234322c
Abhishek Singh <abhishek@gmail.com>
2019-06-18 18:14:45 +0530

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-let score = 1000;
+let score = 100;
 
 if (score >= 1000) {
     score = score + 100;
+}
+else if (score === 1000) {
+    console.log("Almost!");
+}
+else {
+    console.log("Nice try!");
 }
 
 console.log("Score is: ", score);
